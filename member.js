4caacc0ba10d07e782a9e448fe2e99cead3292db
yannickwaelti <47911909+yannickwaelti@users.noreply.github.com>
2023-12-06 09:13:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,27 @@
+function skillsMember() {
+  const skills = [
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'PHP',
+    'SQL',
+    'Python',
+    'Node.js',
+    'Express.js',
+    'React.js',
+    'Vue.js',
+    'Angular.js',
+    'MongoDB',
+    'GraphQL',
+    'Firebase',
+    'AWS',
+    'Docker',
+    'Kubernetes',
+    'Jenkins',
+    'CircleCI',
+    'Heroku',
+    'Git',
+  ];
+
+  return skills;
+}
